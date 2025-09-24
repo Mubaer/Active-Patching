fe157e68a0e8c789da32f766725a334aaa992de4
Peter Ziegler <p.ziegler@mr-daten.de>
2025-09-24 16:21:07 +0200

    wsus summary

--- Onboarding/PSWU-Update.ps1
+++ Onboarding/PSWU-Update.ps1
@@ -1,79 +1,34 @@
-Import-Module pswindowsupdate
-$version = "2.2.6" # added TargetGroups DNS and DHCP
-$Transscript_path = "C:\mr_managed_it\Logs\active_patching." + (Get-Date).ToString("yyyy-MM-dd_HH-mm-ss") + ".txt"
-"Script version: " + $version | Out-File $Transscript_path -Append
-Get-WsusServer -Name localhost -PortNumber 8530
+$assemblyPath = "C:\Program Files\Update Services\Api\Microsoft.UpdateServices.Administration.dll";
+Add-Type -Path $assemblyPath;
 
-#$VMs = get-wsuscomputer -NameIncludes "hpim" | Select-Object fulldomainname
-$MBs = Get-Content -Path "C:\mr_managed_it\Scripts\mbs_hosts.txt"
-$NONADs = Get-Content -Path "C:\mr_managed_it\Scripts\nonad_hosts.txt"
-$TargetGroups = "MR_Server", "MR_Server_SQL", "MR_Server_HV", "MR_Server_DC", "MR_Server_RDS", "MR_Server_CA", "MR_Server_EX", "MR_Server_File", "MR_Server_PR",  "MR_Server_DHCP",  "MR_Server_DNS"
-$ArrayList = $(Get-WsusComputer -ComputerTargetGroups $TargetGroups).FullDomainName
-"List of vms: " + $ArrayList | Out-File $Transscript_path -Append
+$wsusServer = "localhost";
+$csvResult = "C:\mr_managed_it\WSUSSummary.csv";
+$wsus = [Microsoft.UpdateServices.Administration.AdminProxy]::getUpdateServer($wsusServer, $false, 8530);
 
-# Wenn einzelne Server ausgenommen werden sollen, einfach die Zeile 16 nicht auskommentieren und was sinnvolles reinschreiben. Evtl. die Zeile 16 mehrfach verwenden.
-[System.Collections.ArrayList]$VMS = $ArrayList
-#$vms.Remove("mrm-stg-22pswu1.mrm.stg")
-"Removing assets from list ..."  | Out-File $Transscript_path -Append
+$computerscope = New-Object Microsoft.UpdateServices.Administration.ComputerTargetScope;
+$updatescope = New-Object Microsoft.UpdateServices.Administration.UpdateScope;
+$updateSummaries = $wsus.GetSummariesPerComputerTarget($updatescope, $computerscope);
 
-# Get-Date liefert den Tag der Woche im DE-Format, dh. Montag ist 1
-# der ScheduledInstallDay ist aber im US-Format, dh. Sonntag ist 1
-$today = (Get-Date).DayOfWeek.value__  + 1
-"Day code: " + $today | Out-File $Transscript_path -Append
+$results = New-Object System.Collections.ArrayList;
 
-ForEach ($VM in $VMs){
-$EP                      = ""
-$Reboot                  = ""
-$AUOptions               = ""
-$ManagedByMR             = ""
-$ScheduledInstallDay     = ""
+foreach ($summary in $updateSummaries) {
+    $computerTarget = $wsus.GetComputerTarget([guid]$summary.ComputerTargetId);
+    $total = ($summary.NotApplicableCount + $summary.InstalledCount + $summary.NotInstalledCount + $summary.FailedCount);
 
-if ($MBs -match $VM -or $NONADs -match $VM){
+    $entry = @{
+        ComputerTarget                         = $computerTarget.FullDomainName
+        NeededCount                            = ($summary.DownloadedCount + $summary.NotInstalledCount)
+        DownloadedCount                        = $summary.DownloadedCount
+        NotInstalledCount                      = $summary.NotInstalledCount
+        InstalledCount                         = $summary.InstalledCount
+        FailedCount                            = $summary.FailedCount
+        InstalledOrNotApplicablePercentage     = if ($total -ne 0){([Math]::Round((($summary.NotApplicableCount + $summary.InstalledCount) / $total) * 100, 2)).tostring() + '%'}else{[string]0 + '%'}
+    }
 
-"Asset " + $VM + " in exclusion list" | Out-File $Transscript_path -Append}else{
-"Asset name: " + $vm | Out-File $Transscript_path -Append
-
-if (Test-Connection $VM -Count 1 -quiet){
-
-$FDN                     = $VM
-$EP                      = Invoke-Command -ComputerName $FDN {Get-ExecutionPolicy}
-$Reboot                  = Invoke-Command -ComputerName $FDN {Get-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU\' -Name AlwaysAutoRebootAtScheduledTime -ErrorAction SilentlyContinue | Select-Object AlwaysAutoRebootAtScheduledTime}
-$AUOptions               = Invoke-Command -ComputerName $FDN {Get-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU\' -Name AUOptions -ErrorAction SilentlyContinue | Select-Object AUOptions}
-$ManagedByMR             = Invoke-Command -ComputerName $FDN {Get-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\'    -Name ManagedByMR -ErrorAction SilentlyContinue | Select-Object ManagedByMR}
-$ScheduledInstallDay     = Invoke-Command -ComputerName $FDN {Get-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU'  -Name ScheduledInstallDay -ErrorAction SilentlyContinue | Select-Object ScheduledInstallDay}
-
-if($ManagedByMR.ManagedByMR -eq "1" -and $ScheduledInstallDay.ScheduledInstallDay -eq $today){
-
-Set-Item wsman:\localhost\client\TrustedHosts -Value $FDN -Force -Concatenate
-get-Item wsman:\localhost\client\TrustedHosts
-Invoke-Command -ComputerName $FDN -ScriptBlock {Set-ExecutionPolicy RemoteSigned -force}
-
-$Sourcepath = "C:\Program Files\WindowsPowerShell\Modules\PSWindowsUpdate"
-$Destpath   = "\\$FDN\C" + "$" + "\Program Files\WindowsPowerShell\Modules"
-"Copying files ..." | Out-File $Transscript_path -Append
-New-Item -Path $DestPath -ItemType directory -ErrorAction SilentlyContinue
-Copy-Item -Recurse -Path $SourcePath -destination $DestPath -ErrorAction SilentlyContinue
-"Done ..." | Out-File $Transscript_path -Append
-
-Invoke-Command -ComputerName $FDN -ScriptBlock {Import-Module PSWindowsUpdate; Enable-WURemoting -Confirm:$false; Get-WUList}
-
-"Installing Updates ..." | Out-File $Transscript_path -Append
-
-if($Reboot.AlwaysAutoRebootAtScheduledTime -eq "0" -and $AUOptions.AUOptions -eq "2"){
-$FDN + " No Reboot" | Out-File $Transscript_path -Append
-Invoke-WUJob -ComputerName $FDN -Script { Install-WindowsUpdate -AcceptAll -IgnoreReboot } -Confirm:$false -verbose -RunNow
-}else{
-$FDN + " Reboot" | Out-File $Transscript_path -Append
-Invoke-WUJob -ComputerName $FDN -Script { Install-WindowsUpdate -AcceptAll -AutoReboot } -Confirm:$false -verbose -RunNow
-}
-"Updates will be installed offline. Moving on." | Out-File $Transscript_path -Append
-Invoke-Command -ComputerName $FDN {param($EP) Set-ExecutionPolicy $EP} -ArgumentList $EP
-}else{
-"Nothing to do" | Out-File $Transscript_path -Append
+    $results.Add($entry) > $null
 }
 
-}else{
-"Not reachable" | Out-File $Transscript_path -Append
-}
-}
-}
+$resultsArray = $results.ToArray();
+$resultsArray;
+
+foreach ($test in $resultsArray){$test;" "}
