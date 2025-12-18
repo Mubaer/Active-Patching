fac6590e3787ee660d2c2082dfb93b46d1179799
Peter Ziegler <p.ziegler@mr-daten.de>
2025-12-18 16:00:07 +0100

    flight check

--- Onboarding/flight_check verteilen.ps1
+++ Onboarding/flight_check verteilen.ps1
@@ -1,13 +1,17 @@
-$vm = "mrm-stg-22ca01"
+$vms = Get-Content -Path "C:\temp\vms.txt"
+
+foreach($vm in $vms){
+$vm
 $sf = "C:\MR Flightcheck.xml"
 $df = "\\$vm\C" + "$"
-Copy-Item -path $sf -destination $df
+Copy-Item -path $sf -destination $df -force
 
 $sf = "C:\MR_Managed_it\flight_check.ps1"
-$df = "\\$vm\C" + "$" + "\mr_managed_it"
-New-Item -Path $df -ItemType directory
-Copy-Item -path $sf -destination $df
+$df = "\\$vm\C" + "$" + "\mr_managed_it\"
+New-Item -Path $df -ItemType directory -Force
+Copy-Item -path $sf -destination $df -Force
 
 Invoke-Command -ComputerName $vm -ScriptBlock {Register-ScheduledTask -Xml (get-content "C:\MR Flightcheck.xml" | Out-String) -TaskName "MR Flightcheck"
 Remove-Item "C:\MR Flightcheck.xml"
+}
 }
