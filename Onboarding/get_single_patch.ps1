85b571bb16ad4bf69d46ba18f904f06020c35554
Peter Ziegler <p.ziegler@mr-daten.de>
2026-03-05 17:12:39 +0100

    single patch

--- Onboarding/get_single_patch.ps1
+++ Onboarding/get_single_patch.ps1
@@ -24,6 +24,32 @@ If ($request2016.StatusCode -eq 200) {
         }
     }
 }
+$treffer = 0
+$i = 0
+$request2016 = Invoke-WebRequest "https://support.microsoft.com/en-gb/help/4000825" –UseBasicParsing
+If ($request2016.StatusCode -eq 200) {
+    $BuildNumber = [regex]::Matches($request2016.Content, 'href="([a-z0-9-\/]*)">([a-zA-Z]*) ([0-9]{1,2}), ([0-9]{4}).*?(KB[0-9]*) \(OS Build 14393.([0-9]*)\)(?: ([a-zA-Z-]*)<\/a>)?')
+    if ($BuildNumber.Count -gt 0) {
+ 
+        while ($treffer -ne 1 )
+        {
+        if ($buildnumber[$i].Groups[7].value -notlike "Out-of-Band") {
+        $CurrentServer2016Raw = [PSCustomObject]@{
+            'OS Name'      = "Windows Server 2016"
+            'OS Version'   = "14393"
+            'OS build'     = $BuildNumber[$i].Groups[6].Value
+            'KB'           = $BuildNumber[$i].Groups[5].Value
+            'Title'        = ""
+            
+            
+        }
+        $treffer = 1
+            
+        }
+        $i++
+        }
+    }
+}
 
 $kb = $CurrentServer2016Raw.KB
    $updates = $wsus.GetUpdates($updateScope) | Where-Object{$_.Title -match $kb} #Getting every update where the title matches the $kbnumber
@@ -67,17 +93,29 @@ $kb = $CurrentServer2016Raw.KB
         }
      }
 
- 
+$treffer = 0
+$i = 0
  $request2019 = Invoke-WebRequest "https://support.microsoft.com/en-gb/help/4464619" –UseBasicParsing
 If ($request2019.StatusCode -eq 200) {
     $BuildNumber = [regex]::Matches($request2019.Content, 'href="([a-z0-9-\/]*)">([a-zA-Z]*) ([0-9]{1,2}), ([0-9]{4}).*?(KB[0-9]*) \(OS Build 17763.([0-9]*)\)(?: ([a-zA-Z-]*)<\/a>)?')
     if ($BuildNumber.Count -gt 0) {
+ 
+        while ($treffer -ne 1 )
+        {
+        if ($buildnumber[$i].Groups[7].value -notlike "Out-of-Band") {
         $CurrentServer2019Raw = [PSCustomObject]@{
             'OS Name'      = "Windows Server 2019"
             'OS Version'   = "17763"
-            'OS build'     = $BuildNumber[0].Groups[6].Value
-            'KB'           = $BuildNumber[0].Groups[5].Value
+            'OS build'     = $BuildNumber[$i].Groups[6].Value
+            'KB'           = $BuildNumber[$i].Groups[5].Value
             'Title'        = ""
+            
+            
+        }
+        $treffer = 1
+            
+        }
+        $i++
         }
     }
 }
@@ -125,20 +163,32 @@ If ($request2019.StatusCode -eq 200) {
      }
 
  
+$treffer = 0
+$i = 0
      $request2022 = Invoke-WebRequest "https://support.microsoft.com/en-gb/help/5020032" –UseBasicParsing
 If ($request2022.StatusCode -eq 200) {
     $BuildNumber = [regex]::Matches($request2022.Content, 'href="([a-z0-9-\/]*)">([a-zA-Z]*) ([0-9]{1,2}), ([0-9]{4}).*?(KB[0-9]*) \(OS Build 20348.([0-9]*)\)(?: ([a-zA-Z-]*)<\/a>)?')
     if ($BuildNumber.Count -gt 0) {
+ 
+        while ($treffer -ne 1 )
+        {
+        if ($buildnumber[$i].Groups[7].value -notlike "Out-of-Band") {
         $CurrentServer2022Raw = [PSCustomObject]@{
             'OS Name'      = "Windows Server 2022"
             'OS Version'   = "20348"
-            'OS build'     = $BuildNumber[0].Groups[6].Value
-            'KB'           = $BuildNumber[0].Groups[5].Value
+            'OS build'     = $BuildNumber[$i].Groups[6].Value
+            'KB'           = $BuildNumber[$i].Groups[5].Value
             'Title'        = ""
+            
+            
+        }
+        $treffer = 1
+            
+        }
+        $i++
         }
     }
 }
-
         $kb = $CurrentServer2022Raw.KB
    $updates = $wsus.GetUpdates($updateScope) | Where-Object{$_.Title -match $kb} #Getting every update where the title matches the $kbnumber
        ForEach($update in $updates){ #Loop against the list of updates I stored in $updates in the previous step
@@ -182,19 +232,33 @@ If ($request2022.StatusCode -eq 200) {
      }
 
  
+$treffer = 0
+$i = 0
      $request2025 = Invoke-WebRequest "https://support.microsoft.com/en-gb/help/5047442" –UseBasicParsing
 If ($request2025.StatusCode -eq 200) {
     $BuildNumber = [regex]::Matches($request2025.Content, 'href="([a-z0-9-\/]*)">([a-zA-Z]*) ([0-9]{1,2}), ([0-9]{4}).*?(KB[0-9]*) \(OS Build 26100.([0-9]*)\)(?: ([a-zA-Z-]*)<\/a>)?')
     if ($BuildNumber.Count -gt 0) {
+ 
+        while ($treffer -ne 1 )
+        {
+        if ($buildnumber[$i].Groups[7].value -notlike "Out-of-Band") {
         $CurrentServer2025Raw = [PSCustomObject]@{
             'OS Name'      = "Windows Server 2025"
             'OS Version'   = "26100"
             'OS build'     = $BuildNumber[0].Groups[6].Value
             'KB'           = $BuildNumber[0].Groups[5].Value
             'Title'        = ""
+        
+            
+        }
+        $treffer = 1
+            
+        }
+        $i++
         }
     }
 }
+
              $kb = $CurrentServer2025Raw.KB
    $updates = $wsus.GetUpdates($updateScope) | Where-Object{$_.Title -match $kb} #Getting every update where the title matches the $kbnumber
        ForEach($update in $updates){ #Loop against the list of updates I stored in $updates in the previous step
@@ -283,6 +347,3 @@ Write-host
 
   
 Write-host "Check-version: " $version
-
-$LASTEXITCODE = $ExitCode
-exit($exitcode)
--- Onboarding/prepare_modules.ps1
+++ Onboarding/prepare_modules.ps1
@@ -1,11 +1,13 @@
 Import-Module pswindowsupdate
 $version = "2.2.0" # 
-$Transscript_path = "C:\mr_managed_it\Logs\preparing_modules_mbs." + (Get-Date).ToString("yyyy-MM-dd_HH-mm-ss") + ".txt"
+$Transscript_path = "C:\mr_managed_it\Logs\preparing_modules." + (Get-Date).ToString("yyyy-MM-dd_HH-mm-ss") + ".txt"
 Get-WsusServer -Name localhost -PortNumber 8530
 
+$option = Read-Host "(1) Standalone or (2) sek. AD"
+
+if ($option -eq 1){
+
 $VMs = Get-Content -Path "C:\mr_managed_it\Scripts\mbs_hosts.txt"
-
-
 "Version: " + $version | Out-File $Transscript_path -Append
 
 ForEach ($VM in $VMs){
@@ -22,13 +24,10 @@ $Password = $creds.Password
 $user = $s_creds.UserName 
 if ($s_creds){$credential = New-Object System.Management.Automation.PsCredential($user,$securePassword)}
 
-
-
 if ((Test-Connection $VM -Count 1) -and $creds){
 
 $FDN                     = [string]$VM
 Set-Item wsman:\localhost\client\TrustedHosts -Value $FDN -Force -Concatenate
-get-Item wsman:\localhost\client\TrustedHosts | Select-Object Name, Value 
 $EP                      = Invoke-Command -ComputerName $FDN {Get-ExecutionPolicy} -Credential $credential
 $ManagedByMR             = Invoke-Command -ComputerName $FDN {Get-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\'    -Name ManagedByMR -ErrorAction SilentlyContinue | Select-Object ManagedByMR} -Credential $credential
 
@@ -52,3 +51,57 @@ Invoke-Command -ComputerName $FDN -ScriptBlock {
 }
 
 Invoke-Command -ComputerName $FDN {param($EP) Set-ExecutionPolicy $EP} -ArgumentList $EP -Credential $credential
+
+
+}elseif ($option -eq 2){
+
+"Version: " + $version | Out-File $Transscript_path -Append
+$VMs = Get-Content -Path "C:\mr_managed_it\Scripts\nonad_hosts.txt"
+
+ForEach ($VM in $VMs){
+$EP                      = ""
+$ManagedByMR             = ""
+
+"Asset name: " + $vm | Out-File $Transscript_path -Append
+
+$domainuser = Get-Content -Path "C:\mr_managed_it\Scripts\aduser.txt"
+$s_creds = Get-StoredCredential -Target $domainuser
+$creds = Get-StoredCredential -Target $domainuser -AsCredentialObject
+$securePassword = $s_creds.Password
+$Password = $creds.Password
+$user = $s_creds.UserName 
+if ($s_creds){$credential = New-Object System.Management.Automation.PsCredential($user,$securePassword)}
+
+
+
+if ((Test-Connection $VM -Count 1) -and $creds){
+
+$FDN                     = [string]$VM
+Set-Item wsman:\localhost\client\TrustedHosts -Value $FDN -Force -Concatenate
+$EP                      = Invoke-Command -ComputerName $FDN {Get-ExecutionPolicy} -Credential $credential
+$ManagedByMR             = Invoke-Command -ComputerName $FDN {Get-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\'    -Name ManagedByMR -ErrorAction SilentlyContinue | Select-Object ManagedByMR} -Credential $credential
+
+}
+
+if($ManagedByMR.ManagedByMR -eq "1"){
+
+Invoke-Command -ComputerName $FDN -ScriptBlock {Set-ExecutionPolicy RemoteSigned -force} -Credential $credential
+
+"Preparing Modules ..." | Out-File $Transscript_path -Append
+
+Invoke-Command -ComputerName $FDN -ScriptBlock {
+    [Net.ServicePointManager]::SecurityProtocol=[Net.SecurityProtocolType]::Tls12
+    Register-psrepository -Default -ErrorAction SilentlyContinue
+    Install-PackageProvider -Name nuget -MinimumVersion 2.8.5.201 -Force
+    Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+    Install-Module PSWindowsUpdate -Force -SkipPublisherCheck -AllowClobber
+    } -Credential $credential -Verbose
+    }
+
+}
+
+Invoke-Command -ComputerName $FDN {param($EP) Set-ExecutionPolicy $EP} -ArgumentList $EP -Credential $credential
+
+
+
+}else{Write-host "Weder 1 noch 2 gewählt. Ende."}
